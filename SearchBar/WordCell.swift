c31eca95dcb3e5e643ecab0379425f3adb948b95
SeoDongyeon <abocado@seodong-yeon-ui-MacBookPro.local>
2021-12-23 16:43:14 +0900

    first commit

---
+++ SearchBar/WordCell.swift
@@ -0,0 +1,23 @@
+//
+//  WordCell.swift
+//  SearchBar
+//
+//  Created by 아보카도맛엔진오일 on 2021/03/15.
+//
+
+import UIKit
+
+class WordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
